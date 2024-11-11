4e6b4613f058e11d7d9101b03fed1b729ff80e21
ANIFA SUMBA <sumbaanifa@gmail.com>
2024-11-11 13:10:50 +0300

    A java program
    Program to convert temperature in fahreinheit to Celsious using Java

---
+++ FahreinheitToCelsious.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ public class FahreinToCelsious{
+ public static void main(String [] args){
+ 
+ scanner input = new scanner(System.in);
+ System.out.println("Enter value for fahreinheit :");
+ double fahreinheit=input.nextDouble();
+ celsious= (5/9)*(fahreinheit-32);
+ System.out.println(celsiou);
+ }
+  }
+  
+ 
